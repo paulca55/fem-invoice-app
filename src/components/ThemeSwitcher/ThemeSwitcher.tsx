6f6f6851500aee17214f6faae7742c5a6c418131
Paul Cass <7057526+paulca55@users.noreply.github.com>
2023-09-18 21:56:52 +0100

    Add 'ThemeSwitcher' component

---
+++ src/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+import { HalfMoonIcon } from '@/components/svg/icons/HalfMoonIcon';
+import { useThemeMode } from '@/contexts/ThemeModeProvider';
+import { SunIcon } from '@/components/svg/icons/SunIcon';
+
+type AvatarProps = {
+  size?: number;
+};
+
+function ThemeSwitcher({ size = 48 }: AvatarProps) {
+  const { themeMode, toggleThemeMode } = useThemeMode();
+
+  return (
+    <Container $size={size} onClick={toggleThemeMode}>
+      {themeMode === 'light' ? (
+        <HalfMoonIcon color="var(--color-07)" />
+      ) : (
+        <SunIcon color="var(--color-15)" />
+      )}
+    </Container>
+  );
+}
+
+const Container = styled.button<{ $size: number }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: ${(props) => props.$size}px;
+  height: ${(props) => props.$size}px;
+`;
+
+export { ThemeSwitcher };
