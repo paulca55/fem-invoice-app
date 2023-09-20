2b30fdb91fb1f0ec1c6cf0360df942daa7598ded
Paul Cass <7057526+paulca55@users.noreply.github.com>
2023-09-20 19:28:56 +0100

    Add 'Card' component

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+import React from 'react';
+
+type CardProps = {
+  children: React.ReactNode;
+  style?: React.CSSProperties;
+};
+
+function Card({ children, style }: CardProps) {
+  return <StyledContainer style={style}>{children}</StyledContainer>;
+}
+
+const StyledContainer = styled.article`
+  background-color: #fff;
+  border-radius: 8px;
+  box-shadow: hsl(231 38% 45% / 10%) 0 10px 10px -10px;
+`;
+
+export { Card };
