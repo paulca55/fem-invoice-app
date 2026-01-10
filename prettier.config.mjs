31e8eaf02274e6b3cb0c72afbaeaede64c6bfaed
Paul Cass <7057526+paulca55@users.noreply.github.com>
2026-01-10 17:10:44 +0000

    Update ESLint, Prettier and Stylelint versions and config

---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: 'es5',
+  semi: true,
+  singleQuote: true,
+};
+
+export default config;
