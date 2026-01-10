70ca70689926ef7383baaa85c13e3d2650b044a5
Paul Cass <7057526+paulca55@users.noreply.github.com>
2026-01-10 17:10:44 +0000

    Update ESLint, Prettier and Stylelint versions and config

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig, globalIgnores } from 'eslint/config';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+
+export default defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  eslintConfigPrettier,
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
+]);
---
+++ stylelint.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import("stylelint").Config} */
+const config = {
+  extends: ['stylelint-config-standard'],
+  rules: {
+    'value-keyword-case': null,
+  },
+};
+
+export default config;
